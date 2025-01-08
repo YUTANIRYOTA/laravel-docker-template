21a317b2ad000245a5edc8e0555dd57ad28135c0
YUTANIRYOTA <nico0515d@gmail.com>
2025-01-08 15:46:43 +0900

    シーダーの作成

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+      
+        DB::table('todos')->truncate();
+
+        $testData = [
+            [
+                'content' => 'PHP Appセクションを終える',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'content' => 'Laravel Lessonを終える',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+    
+        DB::table('todos')->insert($testData); 
+        {
+            $this->call([
+                TodoSeeder::class,
+            ]);
+        }
+        
         // $this->call(UsersTableSeeder::class);
     }
 }
