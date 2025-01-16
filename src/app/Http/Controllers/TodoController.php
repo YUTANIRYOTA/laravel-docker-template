cc04e171171e532471caef0603acd9468898f4ae
YUTANIRYOTA <nico0515d@gmail.com>
2025-01-16 13:25:56 +0900

    DBに登録されているデータを取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -8,6 +8,9 @@ class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();
+
         return view('todo.index');
     }
 }
