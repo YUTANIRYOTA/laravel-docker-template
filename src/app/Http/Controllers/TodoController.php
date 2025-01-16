6c8b4e9cec00f1c5aad08d12b23b199621759731
YUTANIRYOTA <nico0515d@gmail.com>
2025-01-16 13:42:32 +0900

    取得したデータを画面に渡す

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
@@ -11,6 +11,6 @@ class TodoController extends Controller
         $todo = new Todo();
         $todos = $todo->all();
 
-        return view('todo.index');
+        return view('todo.index', ['todos' => $todos]);    
     }
 }
